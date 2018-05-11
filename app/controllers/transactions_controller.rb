6461a67f5ef4b7815c482193ddb2520824f34e53
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-05-11 12:27:46 +0800

    geo search using backend

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,22 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = if params[:near]
+    @transactions = if params[:l] # l is the upper left and lower right coordinates for map box
+                      sw_lat, sw_lng, ne_lat, ne_lng = params[:l].split(",")
+                      center = Geocoder::Calculations.geographic_center([[sw_lat, sw_lng], [ne_lat, ne_lng]])
+                      distance = Geocoder::Calculations.distance_between(center, [sw_lat, ne_lng])
+                      box      = Geocoder::Calculations.bounding_box(center, distance)
+                      Transaction.within_bounding_box(box) # search in a circle with given radius
+                    elsif params[:near]
                       Transaction.near(params[:near])
                     else
                       Transaction.all
                     end
-    @transactions = @transaction.page(params[:page]).per(5)
+    @transactions = @transactions.page(params[:page]).per(5)
   end
 
+  # hek
+  #
   def show
-    @transaction = Transaction.find(params[:id])
+    @transaction = nil
   end
 end
--- app/javascript/packs/maps/index.js
+++ app/javascript/packs/maps/index.js
@@ -22,9 +22,28 @@ document.addEventListener("turbolinks:load", function() {
         }
       });
 
-      bounds.extend(marker.position);
     }
   });
 
-  map.fitBounds(bounds);
+  let l = document.querySelector("#map").dataset.l;
+  if (l) {
+    // use l which is user specified bounding box coordinates to decide
+    // so boundings do not change before and after one search
+    let latlngs = l.split(",");
+    let southWest = new google.maps.LatLng(latlngs[0], latlngs[1]);
+    let northEast = new google.maps.LatLng(latlngs[2], latlngs[3]);
+    let bounds = new google.maps.LatLngBounds(southWest, northEast);
+    map.fitBounds(bounds, 0); // 0 for removing padding
+  } else {
+    map.fitZoom();
+  }
+
+  document.querySelector("#redo-search").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let bounds = map.getBounds();
+    let location = bounds.getSouthWest().toUrlValue() + "," + bounds.getNorthEast().toUrlValue();
+
+    Turbolinks.visit(`/transactions?l=${location}`);
+  })
 });
